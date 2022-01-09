56f3384f2dd2cbe99348a647a5a13742fa86a51e
Zendai <victorray91@pm.me>
2022-01-09 23:35:29 +0100

    Updates

--- bin/scripts/installStarship.sh
+++ bin/scripts/installStarship.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # From pre-built binary, with Shell.
 # To update simply re-run it.
-sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+sh -c "$(sudo curl -fsSL https://starship.rs/install.sh)"
 
 # Note 
 # The defaults of the install script can be overridden see the built-in help.
@@ -10,11 +10,5 @@ sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 # Add this at the end of ~/.bashrc
 # ~/.bashrc
 # eval "$(starship init bash)"
-
-CHECK='cat ~/.bashrc | grep 'eval "$(starship init bash)"''
-
-if [ "$CHECK" == "" ]; then
-	echo 'eval "$(starship init bash)"' >> ~/.bashrc
-fi
 
 exit
