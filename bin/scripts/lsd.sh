91345b9aa934ddc4892f8e4c40d410e78d98d45d
Victor-ray ( ZendaiOwl ) <victorray91@pm.me>
2022-01-19 17:56:44 +0100

    ࿓❯

--- bin/scripts/lsd.sh
+++ bin/scripts/lsd.sh
@@ -2,19 +2,20 @@
 # My own version of "ls", listing the current working directory's contents.
 # @ZendaiOwl
 white="$(tput setaf 15)"
-direc="$(tput setaf 117)"
+directory="$(tput setaf 117)"
 fil="$(tput setaf 48)"
 red="$(tput setaf 196)"
 reset="$(tput sgr0)"
 for d in ./*
 do 
 	if [ -f "$d" ]; then
-	printf '%s\n' "$fil${d##./}$reset"
+	printf '%-48s' "$fil${d##./}$reset"
 	elif [ -d "$d" ]; then
-	printf '%s\n' "$direc${d##./}$reset"
+	printf '%-48s' "$directory${d##./}$reset"
 	else
-	printf '%s\n' "$red${d##./}$reset"
+	printf '%-48s' "$red${d##./}$reset"
 	fi
-done | column -txc 12
+printf '\n'
+done | column -s '\n'
 
 exit
