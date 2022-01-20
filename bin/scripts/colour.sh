03106540528c9ee0d0b7b81f9cf13845a72e7a3b
Victor-ray ( ZendaiOwl ) <victorray91@pm.me>
2022-01-20 03:05:13 +0100

    ࿓❯

--- bin/scripts/colour.sh
+++ bin/scripts/colour.sh
@@ -103,26 +103,26 @@
 # 
 # 8-16 Colors
 # Color Name	Foreground Color Code	Background Color Code
-# Black	30	40
-# Red	31	41
-# Green	32	42
-# Yellow	33	43
-# Blue	34	44
-# Magenta	35	45
-# Cyan	36	46
-# White	37	47
-# Default	39	49
-# Reset	0	0
+# Black			30						40
+# Red			31						41
+# Green			32						42
+# Yellow		33						43
+# Blue			34						44
+# Magenta		35						45
+# Cyan			36						46
+# White			37						47
+# Default		39						49
+# Reset			0						0
 # 
 # Color Name	Foreground Color Code	Background Color Code
-# Bright Black	90	100
-# Bright Red	91	101
-# Bright Green	92	102
-# Bright Yellow	93	103
-# Bright Blue	94	104
-# Bright Magenta	95	105
-# Bright Cyan	96	106
-# Bright White	97	107
+# Bright Black		90					100
+# Bright Red		91					101
+# Bright Green		92					102
+# Bright Yellow		93					103
+# Bright Blue		94					104
+# Bright Magenta	95					105
+# Bright Cyan		96					106
+# Bright White		97					107
 # 
 # 256 Colors
 # The following escape codes tells the terminal to use the given color ID:
