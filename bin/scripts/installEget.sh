1c4848066c790caf126d96f81993cd22e0921181
Victor-ray ( ZendaiOwl ) <victorray91@pm.me>
2022-01-22 00:24:26 +0100

    ࿓❯

---
+++ bin/scripts/installEget.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+curl -o eget.sh https://zyedidia.github.io/eget.sh
+# Verify the hash
+# shasum -a 256 eget.sh
+# 0e64b8a3c13f531da005096cc364ac77835bda54276fedef6c62f3dbdc1ee919
+echo "Checking SHA256.."
+echo "0e64b8a3c13f531da005096cc364ac77835bda54276fedef6c62f3dbdc1ee919 ./eget.sh" | sha256sum -c
+exit
