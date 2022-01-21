02523bcc8e015de59f72741888a779a2a1242701
Victor-ray ( ZendaiOwl ) <victorray91@pm.me>
2022-01-21 22:50:17 +0100

    ࿓❯

--- bin/scripts/lsd.sh
+++ bin/scripts/lsd.sh
@@ -1,35 +1,74 @@
-#!/bin/bash
-# My own first version of "ls", 
-# listing the current working directory's contents.
-# Green are files, Blue are directories and Red is all other.
+#!/usr/bin/env bash
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# My own first version of "ls" listing the current directory's contents.
+# This script does not show .files, lsda.sh does.
+# It is possible to supply a path to show the contents of as the $1 args.
+# Directories 		: Yellow/Orange
+# Files 		: White
+# Others 		: Red
 # @ZendaiOwl
-# 
-# Color Name	Foreground Color Code	Background Color Code
-# Black			30						40
-# Red			31						41
-# Green			32						42
-# Yellow		33						43
-# Blue			34						44
-# Magenta		35						45
-# Cyan			36						46
-# White			37						47
-# Default		39						49
-# Reset			0						0
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Color Name 		Foreground Color Code
+# Black			30						
+# Red			31						
+# Green			32						
+# Yellow		33						
+# Blue			34
+# Magenta		35
+# Cyan			36
+# White			37
+# Default		39
+# Reset			0
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Colour 		Background Color Code
+# Black			40
+# Red			41
+# Green			42
+# Yellow		43
+# Blue			44
+# Magenta		45
+# Cyan			46
+# White			47
+# Default		48
+# Reset			0
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 RD='\033[0;31m'
 GR='\033[0;32m'
+YW='\033[0;33m'
 BL='\033[0;34m'
+MG='\033[0;35m'
+WT='\033[0;39m'
 NC='\033[0;0m'
-for d in ./*
-do 
-	if [ -f "$d" ]; then
-		OUT="$GR${d##./}$NC"
-	elif [ -d "$d" ]; then
-		OUT="$BL${d##./}$NC"
+dir=$1
+if [[ -z $1 ]]; then
+	for d in ./*
+	do 
+		if [[ -d $d ]]; then
+			OUT="$YW${d##*/}$NC"
+		elif [[ -f $d ]]; then
+			OUT="$WT${d##*/}$NC"
+		else
+			OUT="$RD${d##*/}$NC"
+		fi
+	# printf "%b$OUT%b\n"
+	printf '%b%s%b\n' "$OUT"
+	done | column
+else
+	if [[ -e $1 ]]; then
+		for d in "$1"/*
+		do
+			if [[ -d $d ]]; then
+				OUT="$YW${d##*/}$NC"
+			elif [[ -f $d ]]; then
+				OUT="$WT${d##*/}$NC"
+			else
+				OUT="$RD${d##*/}$NC"
+			fi
+		printf '%b%s%b\n' "$OUT"
+		done | column
 	else
-		OUT="$RD${d##./}$NC"
+		printf '%s\n' "$1: Directory not found."
 	fi
-# printf "%b$OUT%b\n"
-printf '%b%s%b\n' "$OUT"
-done | column
+fi
 
 exit
--- bin/scripts/lsda.sh
+++ bin/scripts/lsda.sh
@@ -1,14 +1,40 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # @ZendaiOwl 
 # This is my second take on writing my own 'ls' function.
-# This script prints the content of the specified directory.
+# This script prints the content of the specified directory, not current.
 # The 1st given argument is processed as the specified directory to print.
-# Some colours below
+# This script also prints .files.
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Color Name	Foreground Color Code
+# Black			30						
+# Red			31						
+# Green			32						
+# Yellow		33						
+# Blue			34
+# Magenta		35
+# Cyan			36
+# White			37
+# Default		39
+# Reset			0
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Colour 		Background Color Code
+# Black			40
+# Red			41
+# Green			42
+# Yellow		43
+# Blue			44
+# Magenta		45
+# Cyan			46
+# White			47
+# Default		48
+# Reset			0
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 RD='\033[0;31m'
 GR='\033[0;32m'
 YW='\033[0;33m'
 BL='\033[0;34m'
-WT='\033[0;37m'
+WT='\033[0;39m'
 NC='\033[0;0m'
 dir="$1"
 # dir="${dir:=$HOME}"
