63e36e378ab562cdfcafaedadf2c94bcd9e9ded6
ZendaiOwl <victorray91@pm.me>
2022-01-29 01:22:38 +0000

    ࿓❯

--- bin/scripts/lsd.sh
+++ bin/scripts/lsd.sh
@@ -33,17 +33,13 @@
 # Default		48
 # Reset			0
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# RD=$'\e[0;31m'
-# GR=$'\e[0;32m'
-# GR=$'\e[0;32m'
-# BL=$'\e[0;34m'
-# MG=$'\e[0;35m'
-# WT=$'\e[0;39m'
-# NC=$'\e[0m'
-YW=$(tput setaf 208)
-WT=$(tput setaf 15)
-RD=$(tput setaf 198)
-NC=$(tput sgr 0)
+RD=$'\e[31m'
+GR=$'\e[32m'
+GR=$'\e[32m'
+BL=$'\e[34m'
+MG=$'\e[35m'
+WT=$'\e[39m'
+NC=$'\e[0m'
 dir=$1
 if [[ ! -e $1 ]]; then
 	for d in ./*
@@ -51,11 +47,11 @@ if [[ ! -e $1 ]]; then
 		if [[ -d $d ]]; then
 			OUT="$YW${d##*/}$NC"
 		elif [[ -f $d ]]; then
-			OUT="$WT${d##*/}$NC"
+			OUT="${d##*/}"
 		else
 			OUT="$RD${d##*/}$NC"
 		fi
-	printf '%-0.24s\n' "$OUT"
+	printf '%b%-0.24s\n' "$OUT"
 	done | column
 else
 	if [[ -e $1 ]]; then
@@ -64,11 +60,11 @@ else
 			if [[ -d $d ]]; then
 				OUT="$YW${d##*/}$NC"
 			elif [[ -f $d ]]; then
-				OUT="$WT${d##*/}$NC"
+				OUT="${d##*/}"
 			else
 				OUT="$RD${d##*/}$NC"
 			fi
-		printf '%-4.24s\n' "$OUT"
+		printf '%b%-5.24s \n' "$OUT"
 		done | column
 	else
 		printf '%-0.24s\n' "$1: Directory not found."
