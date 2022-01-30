7111dad68a985b66d5945eb486bec2121492da61
Zendai <victorray91@pm.me>
2022-01-30 18:15:47 +0100

    ࿓❯

--- bin/scripts/lsd.sh
+++ bin/scripts/lsd.sh
@@ -44,17 +44,17 @@ if [[ -z $dir ]] ; then
 		if [[ -d $d ]] ; then
 			printf '\e[32m%-12.48s\e[0m\n' "${d##*/}"
 		elif [[ -x $d ]] ; then
-			printf '\e[36m%-12.48s\e[0m\n' "${d##*/}"
+			printf '\e[33m%-12.48s\e[0m\n' "${d##*/}"
 		# elif [[ -w $d ]] ; then
 			# printf '\e[32m%-12.48s\e[0m\n' "${d##*/}"
 		elif [[ -S $d ]] ; then
-			printf '\e[35m%-12.48s\e[0m\n' "${d##*/}"
+			printf '\e[36m%-12.48s\e[0m\n' "${d##*/}"
 		elif [[ -f $d ]] ; then
-			printf '\e[37m%-12.48s\e[0m\n' "${d##*/}"
+			printf '\e[39m%-12.48s\e[0m\n' "${d##*/}"
 		elif [[ -e $d ]] ; then
 			printf '\e[34m%-12.48s\e[0m\n' "${d##*/}"
 		else
-			printf '\e[33m%-12.48s\e[0m\n' "${d##*/}"
+			printf '\e[31m%-12.48s\e[0m\n' "${d##*/}"
 		fi
 	done | column
 else
@@ -64,17 +64,17 @@ else
 		if [[ -d $d ]] ; then
 			printf '\e[32m%-12.48s\e[0m\n' "${d##*/}"
 		elif [[ -x $d ]] ; then
-			printf '\e[36m%-12.48s\e[0m\n' "${d##*/}"
+			printf '\e[33m%-12.48s\e[0m\n' "${d##*/}"
 		# elif [[ -w $d ]] ; then
 			# printf '\e[32m%-12.48s\e[0m\n' "${d##*/}"
 		elif [[ -S $d ]] ; then
-			printf '\e[35m%-12.48s\e[0m\n' "${d##*/}"
+			printf '\e[36m%-12.48s\e[0m\n' "${d##*/}"
 		elif [[ -f $d ]] ; then
-			printf '\e[37m%-12.48s\e[0m\n' "${d##*/}"
+			printf '\e[39m%-12.48s\e[0m\n' "${d##*/}"
 		elif [[ -e $d ]] ; then
 			printf '\e[34m%-12.48s\e[0m\n' "${d##*/}"
 		else
-			printf '\e[33m%-12.48s\e[0m\n' "${d##*/}"
+			printf '\e[31m%-12.48s\e[0m\n' "${d##*/}"
 		fi
 		done | column
 	else
