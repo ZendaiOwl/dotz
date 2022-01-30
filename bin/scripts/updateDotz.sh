539f2b796222cbefb05024b1aca9cbb1f5550ccd
Zendai <victorray91@pm.me>
2022-01-30 18:21:10 +0100

    ࿓❯

---
+++ bin/scripts/updateDotz.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Updates my Dotz.git dotfiles repository
+# For my dotfiles, configurations and scripts
+dotz="/usr/bin/git --git-dir=$HOME/.dotz/ --work-tree=$HOME"
+${dotz} add /home/zendai/bin/scripts/*
+${dotz} commit -m "࿓❯"
+${dotz} push
+exit
