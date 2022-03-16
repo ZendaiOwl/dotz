eea23721157e5e9d98e8db96b7e1fd217976d644
Zendai Owl <victorray91@pm.me>
2022-03-16 14:34:56 +0100

    ࿓❯ Updating Dotz scripts
    Signed-off-by: Zendai Owl <victorray91@pm.me>

--- bin/scripts/updateDotz.sh
+++ bin/scripts/updateDotz.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 # Updates my Dotz.git dotfiles repository
 # For my dotfiles, configurations and scripts
 dotz="/usr/bin/git --git-dir=$HOME/.dotz/ --work-tree=$HOME"
-${dotz} add /home/zendai/bin/scripts/*
-${dotz} add ${HOME}/.profile
-${dotz} add ${HOME}/.bash_aliases
-${dotz} commit -m "࿓❯ Updating Dotz scripts"
-${dotz} push
+${dotz} add /home/zendai/bin/scripts/* || exit 1
+${dotz} add ${HOME}/.profile || exit 1
+${dotz} add ${HOME}/.bash_aliases || exit 1
+${dotz} commit -s -m "࿓❯ Updating Dotz scripts" || exit 1
+${dotz} push || exit 1
 exit
